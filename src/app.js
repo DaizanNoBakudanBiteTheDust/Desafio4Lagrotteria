2334bfac7847f095af091b728bf63d167a0ecf85
Gabriel Lagrotteria <gabriellagrotteria18@gmail.com>
2023-10-06 12:00:25 -0300

    Socket io agregado

--- src/app.js
+++ src/app.js
@@ -1,17 +1,28 @@
 import express from 'express';
 import handlebars from 'express-handlebars';
-import { __dirname } from './utils.js'
+import {
+        __dirname
+} from './utils.js'
+import {
+        Server
+} from 'socket.io';
 import productRouter from './routes/api/products.router.js';
 import cartRouter from './routes/api/cart.router.js';
+import viewRouter from './routes/web/views.router.js';
 
 // Crea server express
 const app = express();
+
+//Servidor archivos estaticos
+
+app.use(express.static(`${__dirname}/public`))
 
 //middleware
 app.use(express.json({}));
 app.use(express.urlencoded({
         extended: true
 }));
+
 
 // handlebars
 
@@ -20,15 +31,19 @@ app.set('views', `${__dirname}/views`)
 app.set('view engine', 'handlebars')
 
 
-app.use(express.static('public'))
-
 // Llama a la ruta de product Router (Todo lo hecho hasta ahora)
 app.use('/api/products', productRouter);
 
 // Ruta carts
 app.use('/api/carts', cartRouter);
 
+// Ruta view
+app.use('/web/views', viewRouter);
 
 
-app.listen(8080, () => console.log("listening en 8080"));
 
+const Server = app.listen(8080, () => console.log("listening en 8080"));
+
+// IO
+
+const socketServer = new Server();
--- src/routes/web/views.router.js
+++ src/routes/web/views.router.js
@@ -2,13 +2,13 @@ import {
     Router
 } from 'express'
 
-import ProductManager from '../../manager/productManager.js';
+import ProductManager from '../../managers/productManager.js';
 
 const router = Router()
 const productManager = new ProductManager();
 
 router.get('/', async (req, res) => { 
-    res.render('realTimeProducts', { products: productManager.listarAll() });
+    res.render('realTimeProducts', { products: productManager.getProducts() });
 });
 
 export default router;
